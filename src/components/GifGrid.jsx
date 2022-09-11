b91933398c84cf1eb108cb297dc02787a42eb2ee
Heissen Lopez <heissen.lopez@gmail.com>
2022-09-10 19:52:46 -0500

    Add useState and useEffect hooks to the app

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs"
+
 export const GifGrid = ({ category }) => {
+
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, []);
+
   return (
     <>
       <h3>{ category }</h3>
-      <p>Hell world!</p>
+      {
+
+      }
     </>
   )
 }
