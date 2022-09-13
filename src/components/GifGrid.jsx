556ccfd375384a04fc0fde83f283a17143abde64
Heissen Lopez <heissen.lopez@gmail.com>
2022-09-13 01:20:23 -0500

    Update GifFrid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs"
+
+import { GridItem } from "./GridItem";
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
@@ -16,13 +18,16 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{ category }</h3>
-      <ol>
+      <div  className="card-grid" >
         {
-          images.map(({id, title}) => (
-            <li key={ id }>{ title }</li>)
+          images.map((image) => (
+            <GridItem
+              key = { image.id }
+              { ...image }
+            />)
           )
         }
-      </ol>
+      </div>
     </>
   )
 }
