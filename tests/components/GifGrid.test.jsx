b81d669f537bdc564c6bd5c84d859040aedd6ba6
Heissen Lopez <heissen.lopez@gmail.com>
2023-12-07 00:00:46 -0500

    Include jest.mock support and use mockReturnValue in tests

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,13 +1,45 @@
 import { screen, render } from '@testing-library/react';
 import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Test on <GifGrid />', () => {
   const category = 'Breaking Bad';
 
   test('Should display the initial loading', () => {
+
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    })
+
     render(<GifGrid category={ category }/>);
     expect(screen.getByText('Is loading...'))
     expect(screen.getByText(category))
-  });  
+  });
+
+  test('Should display items when images loading useFetchGifs', () => {
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Haminne no Ippo',
+        url: 'https://test.page/ippo,jpg'
+      },
+      {
+        id: '1234',
+        title: 'Goku',
+        url: 'https://test.page/goku.jpg'
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    })
+
+    render( <GifGrid category = { category } />);
+    expect( screen.getAllByRole('img').length).toBe(2);
+  });
 });
 
